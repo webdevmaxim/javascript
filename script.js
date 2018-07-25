0606c36a93d1a0189448fdc4e2996d91ee2a0994
max <maxi10t@mail.ru>
2018-07-25 10:21:46 +0200

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,41 @@
+/*Переменные*/
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var main = document.getElementById("main");
+var myList = document.getElementById("list");
+var ul = document.getElementsByTagName("ul");
+var story = myList.getElementsByClassName("item");
+var popup = document.getElementsByClassName("popup")[0];
+var close = document.querySelector(".close");
+
+/*Переменные выводим в консоль*/
+console.log(btn1); 
+console.log(btn2); 
+console.log(ul); 
+console.log(story); 
+
+
+/*Функции*/
+function addItem(){
+	var newLi = document.createElement("li");
+	newLi.innerHTML = "Новая задача!";
+	newLi.className = "item";
+	myList.appendChild(newLi);
+	popup.style.display = "none";
+}
+function delItem(){
+	myList.removeChild(story[0]);
+
+	if(story.length == 0){
+		popup.style.display = "block"
+	}
+
+}
+
+function closePopup(){
+	 popup.style.display = "none";
+}
+/*События*/
+btn1.addEventListener("click", addItem);
+btn2.addEventListener("click", delItem);
+close.addEventListener("click", closePopup);
